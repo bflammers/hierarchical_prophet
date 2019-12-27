e5b1c6367616592d5f80697c17cac8e9a2f8a223
Bart Lammers <bartlammers@Barts-MBP.home>
2019-12-27 23:27:41 +0100

    Model fully hierarchical - not working cleanly yet

--- src/utils.py
+++ src/utils.py
@@ -2,20 +2,26 @@
 import numpy as np
 import pandas as pd
 import datetime
-import scipy
+
+
+days_in_year = 365.25
+days_in_month = days_in_year / 12
 
 
 class MinMaxScaler:
 
-    def __init__(self):
+    def __init__(self, centering=True, scaling=True):
+
+        self.centering=centering
+        self.scaling=scaling
 
         self.loc = None
         self.scale = None
 
     def fit(self, x):
 
-        self.loc = min(x)
-        self.scale = max(x) - min(x)
+        self.loc = min(x) if self.centering else 0
+        self.scale = max(x) - min(x) if self.centering else 1
 
     def transform(self, x):
 
@@ -27,54 +33,92 @@ class MinMaxScaler:
         return self.transform(x)
 
 
-def random_timeseries(n_series = 5, n_years = 5):
+def random_walk(n, start=0, sigma=0.2):
+
+    x = np.ndarray(n)
+    x[0] = start
+    
+    for i in range(1, n):
+        x[i] = x[i-1] + np.random.normal(scale=sigma)
+
+    return x
+
+
+def random_seasonality(time, yearly=True, monthly=False, weekly=False):
+
+    x = np.zeros(len(time))
+
+    if yearly:
+        x += np.sin(time / days_in_year * 2 * np.pi * np.random.uniform(low=.5, high=1.5))  # Yearly period seasonality
+
+    if monthly:
+        x += np.sin(time / days_in_month * 2 * np.pi * np.random.uniform(low=.5, high=1.5))  # Yearly period seasonality
+
+    if weekly:
+        x += np.sin(time / 7 * 2 * np.pi * np.random.uniform(low=.5, high=1.5)) # Weekly period seasonality
+
+    return x
+
+
+def random_timeseries(n_series = 5, n_years = 5, equal_trend=False, equal_start=False, 
+    equal_seasonality=False, equal_error=False):
     """Function that generated a dataframe with correlated timeseries
     
     Keyword Arguments:
         n_series {int} -- Number of series (default: {5})
         n_years {int} -- Number of years (default: {5})
+        equal_trend = boolean indicating if trend should be equal for all series
+        equal_start = boolean indicating if starting point should be equal for all series
+        equal_seasonality = boolean indicating if seasonality should be equal for all series
+        equal_error = boolean indicating if the variance of the error terms should be equal
     
     Returns:
         pandas.DataFrame -- Dataframe with timeseries
     """
-    # Constant
-    days_in_year = 365.25
     
     # Generate time array
-    t = np.arange(n_years * days_in_year)
+    time = np.arange(n_years * days_in_year)
     
     # Generate dataframe with datecolumn and t column
     today = datetime.datetime.now().date()
-    start = today - datetime.timedelta(days = len(t) - 1)
+    start = today - datetime.timedelta(days = len(time) - 1)
     
     df = pd.DataFrame({
         'ds': pd.date_range(start=start, end=today, freq='D'), 
-        't': t
+        't': time
     })
-    
-    # Generate base series
-    y_1 = np.sin(t / days_in_year * np.pi) * 3 # Yearly period seasonality
-    y_1 += np.sin(t / days_in_year * 12 * np.pi) * 2 # Monthly period seasonality
-    y_1 += np.sin(t / 7 * np.pi) # Weekly period seasonality
-    y_1 += t / days_in_year # Trend
-    y_1 += scipy.random.normal(size=len(t))
-    
-    # Construct df
-    df['y_1'] = y_1
+
+    # Construct starting point and trend
+    start = 0 
+    trend = random_walk(n=len(time))
+    seasonality = random_seasonality
+    error_sd = 1
     
     # Add random walks to base series
     for i in range(n_series):
-        
-        rw = scipy.random.normal(scale=5)
-        mu = scipy.random.normal(scale=0.01)
-        y_i = y_1.copy()
-        
-        for j in range(len(t)):
 
-            rw += scipy.random.normal(loc=mu, scale=0.2)
-            y_i[j] += rw
-            
-        df['y_{}'.format(i)] = y_i
+        if not equal_start:
+            start = np.random.uniform(low=-3, high=3)
+        
+        if not equal_trend:
+            trend = random_walk(n=len(time))
+
+        if not equal_seasonality:
+            seasonality = random_seasonality(time)
+
+        if not equal_error:
+            error_sd = np.random.uniform(high=2)
+    
+        # Generate base series
+        y = start + seasonality # trend + seasonality
+        # y += np.random.normal(size=len(time), scale=error_sd) # Error term
+        
+        # Construct df
+        df['y_{}'.format(i)] = y
     
     return df
     
+if __name__=='__main__':
+
+    df = random_timeseries()
+    print(df)
